88b43548236629b98d1cc62370bd8448dc6e3049
Nicolas Leport <nicolas.leport@nkcreation.com>
2021-01-08 14:32:27 +0100

    fix: remove base content

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
     <Hero />
-    <div>
-      <h1 class="title">nkcreation</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
