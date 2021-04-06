7a70bdbc8774ab463cfcb6b4ab773f6bd58ab567
Nicolas Leport <nicolas.leport@nkcreation.com>
2021-04-06 10:43:37 +0200

    feat: add head meta infos

--- pages/index.vue
+++ pages/index.vue
@@ -7,5 +7,17 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  head: {
+    title: 'nkCreation - WebDesigner & Developer in Rennes, FR (35)',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Nicolas Leport, designer and interface builder in Rennes, France. Actually work for Zenika. Also a freelancer and CEO at Skypaper.',
+      },
+    ],
+  },
+})
 </script>
