226890579de2e69910daaccc57e16187f0c81b08
Nicolas Leport <nicolas.leport@nkcreation.com>
2021-08-04 19:12:22 +0200

    feat: plug directus

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,52 +1,68 @@
 <template>
-  <div class="hero">
+  <section class="hero">
+    <span class="background"></span>
     <span class="bg-grid"></span>
 
     <div class="u-wrapper">
-      <header>
-        <Logo />
-      </header>
-
       <main>
-        <h1>
-          Hi. I’m Nicolas Leport.<br />
-          I design & build user interfaces.
-        </h1>
+        <h1 data-disappear v-html="title"></h1>
 
-        <p>
-          Based in Rennes (35), France, I currently work for
-          <strong>Zenika</strong> as a consultant.<br />
-          I’m also freelancer and CEO at
-          <a rel="noopener" href="https://skypaper.io/" target="_blank"
-            ><strong>Skypaper</strong></a
-          >.
-        </p>
+        <div data-disappear class="content" v-html="message"></div>
 
         <Button
-          href="https://nicolasleport.fr/contact/"
+          v-if="link && label"
+          data-disappear
+          :href="hrefLink"
           target="_blank"
           rel="noopener"
           class="button"
           color="white"
-          >Let's work together</Button
+          >{{ label }}</Button
         >
       </main>
     </div>
-  </div>
+  </section>
 </template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class HeroComponent extends Vue {
+  @Prop({ type: String, default: '' }) readonly title!: string
+  @Prop({ type: String, default: '' }) readonly message!: string
+  @Prop({ type: String, default: '' }) readonly link!: string
+  @Prop({ type: String, default: '' }) readonly label!: string
+
+  get hrefLink() {
+    return `https://nicolasleport.fr/${this.link}`
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .hero {
-  background: url('~assets/img/background.svg');
-  background-size: cover;
-  background-position: center center;
   height: 100vh;
   width: 100%;
   position: relative;
+  padding-top: 8.8rem + 4.8rem;
+  overflow: hidden;
 }
-header {
-  margin-top: 8.8rem;
+
+.background {
+  --parallax-speed: 0.5;
+
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url('~assets/img/background.svg');
+  background-size: cover;
+  background-position: center center;
 }
+
 .u-wrapper {
   height: 100%;
   display: flex;
@@ -55,7 +71,23 @@ header {
   justify-content: center;
   position: relative;
   z-index: 2;
+
+  h1 {
+    --fade-begin: 100;
+    --fade-end: 390;
+  }
+
+  .content {
+    --fade-begin: 260;
+    --fade-end: 400;
+  }
+
+  .button {
+    --fade-begin: 360;
+    --fade-end: 500;
+  }
 }
+
 main {
   flex: 1 1 auto;
   display: flex;
@@ -73,16 +105,18 @@ h1 {
   margin-top: 0;
 }
 
-a {
-  font: inherit;
-  color: inherit;
-}
+.content ::v-deep {
+  a {
+    font: inherit;
+    color: inherit;
+  }
 
-p {
-  font-size: clamp(1.6rem, 1.3231rem + 0.7692vw, 2.4rem);
-  font-weight: normal;
-  color: rgba(white, 0.88);
-  margin: 2em 0;
+  p {
+    font-size: clamp(1.6rem, 1.3231rem + 0.7692vw, 2.4rem);
+    font-weight: normal;
+    color: rgba(white, 0.88);
+    margin: 2em 0;
+  }
 }
 
 .button {
