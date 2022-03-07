e346c290a7db9525b90cf79425fb59c1ee88594c
Nicolas Leport <nicolas.leport@nkcreation.com>
2022-03-07 17:12:57 +0100

    feat: add page title component

---
+++ components/ui/atoms/PageTitle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1 :class="{ 'has-subtitle': hasSubtitle }">
+      <slot name="title">{{ title }}</slot>
+    </h1>
+    <p v-if="hasSubtitle" class="subtitle">
+      <slot name="subtitle">{{ subtitle }}</slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    hasSubtitle() {
+      return this.subtitle || this.$slots?.subtitle?.length > 0
+    },
+  },
+}
+</script>
+
+<style scoped>
+.subtitle {
+  width: calc(var(--background-gutter-size) * 5);
+}
+</style>
